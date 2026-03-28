901a232fa1636f9ea520faa3e43f954c6bddb15e
mikun <mikundas2000@gmail.com>
2026-03-28 17:41:31 +0530

    changing in requirement.txt

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://ai-emotion-based-music-content-uqhq.onrender.com";
+const API = "https://ai-emotion-based-music-content-1fha.onrender.com";
 
 export const detectEmotion = async (formData) => {
   return await axios.post(`${API}/detect`, formData);
