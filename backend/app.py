f3f18eb23a8ef0001265bf1071795d96da21c590
mikun <mikundas2000@gmail.com>
2026-03-28 22:00:19 +0530

    backend changed

--- backend/app.py
+++ backend/app.py
@@ -71,4 +71,4 @@ def detect():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
